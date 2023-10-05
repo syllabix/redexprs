c0bb57a2cb3554283e76f1554125eb02e957f4e2
Tom Stoepker <tom.stoepker@gmail.com>
2023-10-05 21:15:47 +0200

    task: setup basic local dev environment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Error, ErrorKind, Result};
+
+use redis::{cluster::ClusterClient, AsyncCommands};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let nodes = vec!["redis://bitnami@localhost:6379/"];
+    let client = ClusterClient::new(nodes).map_err(|e| Error::new(ErrorKind::Other, e))?;
+    let mut conn = client
+        .get_async_connection()
+        .await
+        .map_err(|e| Error::new(ErrorKind::NotConnected, e))?;
+
+    conn.set("test", "test_data")
+        .await
+        .map_err(|e| Error::new(ErrorKind::Other, e))?;
+
+    let result: String = conn
+        .get("test")
+        .await
+        .map_err(|e| Error::new(ErrorKind::Other, e))?;
+
+    println!("retrieved result from redis: {result}");
+
+    Ok(())
 }
