9d74a0c5090e023cba18bbd291072bac3c6d8027
Tom Stoepker <tom.stoepker@gmail.com>
2023-10-07 20:53:25 +0200

    feat: working local redis cluster via docker compose

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # redexprs
-simple experiments with redis and rust
+simple experiments with redis and rust, with a specific attention to working in cluster mode and with connection pooling.
+
+## get started
+
+1. Install [Rust](https://www.rust-lang.org/tools/install)
+2. Install [Docker](https://docs.docker.com/engine/install/)
+
+Once project dependencies are installed, start up the development environment (a local sharded redis cluster) with `docker compose up`
+
+Run the example with `cargo run`
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use redis::{cluster::ClusterClient, AsyncCommands};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let nodes = vec!["redis://bitnami@localhost:6379/"];
+    println!("starting up!");
+    let nodes = vec!["redis://127.0.0.1:7000"];
     let client = ClusterClient::new(nodes).map_err(|e| Error::new(ErrorKind::Other, e))?;
     let mut conn = client
         .get_async_connection()
         .await
         .map_err(|e| Error::new(ErrorKind::NotConnected, e))?;
+
+    println!("connection establish - test set and get methods next");
 
     conn.set("test", "test_data")
         .await
